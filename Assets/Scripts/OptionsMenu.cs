ef8911cce26ece76c65aa1a6ac6648af451ca94f
Luca Steccanella <luca.steccanella@live.it>
2020-06-08 18:03:14 +0200

    Finished menu

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMaster(float volume)
+    {
+        SetVolume("MasterVolume", volume);
+    }
+
+    public void SetMusic(float volume)
+    {
+        SetVolume("MusicVolume", volume);
+    }
+
+    public void SetSFX(float volume)
+    {
+        SetVolume("SFXMasterVolume", volume);
+    }
+    private void SetVolume(string value, float volume)
+    {
+        audioMixer.SetFloat(value, volume);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
