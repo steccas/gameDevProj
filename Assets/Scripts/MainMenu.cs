b58ea726c0dc97ad1a6ae8adb6712b06d189de00
Luca Steccanella <luca.steccanella@live.it>
2020-06-09 16:08:43 +0200

    Loading Bar

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider loadingBar;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadGame());
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private IEnumerator LoadGame()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        
+        while (!operation.isDone)
+        {
+            loadingBar.value = operation.progress;
+            yield return null;
+        }
     }
 }
