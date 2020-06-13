324813fda05a8979fb280fbe193dc556c477d571
Luca Steccanella <luca.steccanella@live.it>
2020-06-13 14:13:49 +0200

    Map fixes

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -7,19 +7,27 @@ public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    private void Start()
+    {
+       
+    }
+
     public void SetMaster(float volume)
     {
         SetVolume("MasterVolume", volume);
+        SetPref("MasterVolume", volume);
     }
 
     public void SetMusic(float volume)
     {
         SetVolume("MusicVolume", volume);
+        SetPref("MusicVolume", volume);
     }
 
     public void SetSFX(float volume)
     {
         SetVolume("SFXMasterVolume", volume);
+        SetPref("SFXMasterVolume", volume);
     }
     private void SetVolume(string value, float volume)
     {
@@ -28,6 +36,16 @@ public class OptionsMenu : MonoBehaviour
 
     public void SetFullScreen (bool isFullScreen)
     {
+        int pref;
+        if (isFullScreen) pref = 1;
+        else pref = 0;
         Screen.fullScreen = isFullScreen;
+        PlayerPrefs.SetInt("Fullscreen", pref);
+        PlayerPrefs.Save();
+    }
+    void SetPref(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
     }
 }
